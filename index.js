9945e4db6c68266b38768ec1745b66a13e40b071
Veronica Miller <vmcmiller@gmail.com>
2019-02-20 11:20:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,19 @@ function BoardMember(name, homeState, training) {
   this.name = name;
   this.homeState = homeState;
   this.training = training;
+  this.veto = function() {
+    "No, I must disagree";
+  };
+  this.approve = function() {
+    "You can do that!";
+  };
+  this.doCharity = function() {
+    "I like to help people";
+  };
+  this.releasePressStatement = function() {
+    "You will see great things from Scuber.";
+  };
+  this.sayHi = function() {
+    `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+  };
 };
