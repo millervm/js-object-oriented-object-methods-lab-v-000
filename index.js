0e503adddc1ecb0c28dd8a2a9a706d5609d6c591
Veronica Miller <vmcmiller@gmail.com>
2019-02-20 11:20:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function BoardMember(name, homeState, training) {
   this.homeState = homeState;
   this.training = training;
   this.veto = function() {
-    "No, I must disagree";
+    return "No, I must disagree";
   };
   this.approve = function() {
     "You can do that!";
