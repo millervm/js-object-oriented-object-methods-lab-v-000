4766ead31f182423483d098faf4633a01655cf61
Veronica Miller <vmcmiller@gmail.com>
2019-02-20 11:20:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ function BoardMember(name, homeState, training) {
     return "No, I must disagree";
   };
   this.approve = function() {
-    "You can do that!";
+    return "You can do that!";
   };
   this.doCharity = function() {
-    "I like to help people";
+    return "I like to help people";
   };
   this.releasePressStatement = function() {
-    "You will see great things from Scuber.";
+    return "You will see great things from Scuber.";
   };
   this.sayHi = function() {
-    `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   };
 };
