275c6cf3a0b1643bd2953be2ca5e82e84c24b43c
Veronica Miller <vmcmiller@gmail.com>
2019-02-20 11:14:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,4 @@ function BoardMember(name, homeState, training) {
   this.name = name;
   this.homeState = homeState;
   this.training = training;
-  this.veto = function {};
-  this.approve = function {};
-  this.doCharity = function {};
-  this.releasePressStatement = function {};
-  this.sayHi = function {};
 };
