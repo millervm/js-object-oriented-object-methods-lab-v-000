a005487179f6b4fc052a5859a4d4ae3a8a8efa96
Veronica Miller <vmcmiller@gmail.com>
2019-02-20 11:11:04 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,7 @@
+function boardMember() {
+  this.veto = function {};
+  this.approve = function {};
+  this.doCharity = function {};
+  this.releasePressStatement = function {};
+  this.sayHi = function {};
+};
